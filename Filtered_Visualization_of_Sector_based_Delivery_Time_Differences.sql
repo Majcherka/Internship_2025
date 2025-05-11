ad72b0cb286dde050e4192ee4e931cc3c36385d6
Majcherka <magdulka2003@gmail.com>
2025-05-11 21:55:45 +0200

    Add SQL code for Data Analysis and Visualisation task

---
+++ Filtered_Visualization_of_Sector_based_Delivery_Time_Differences.sql
@@ -0,0 +1,13 @@
+WITH delivery_times AS (
+   SELECT o.sector_id, TIMESTAMPDIFF(SECOND, rs.segment_start_time, rs.segment_end_time) / 60 AS avg_delivery_time
+   FROM route_segments rs
+            JOIN orders o ON rs.order_id = o.order_id
+   WHERE rs.segment_type = 'STOP'
+),
+ranked AS (
+   SELECT *,
+          NTILE(100) OVER (PARTITION BY sector_id ORDER BY avg_delivery_time) AS bucket
+   FROM delivery_times
+)
+SELECT sector_id, AVG(avg_delivery_time) FROM ranked WHERE bucket BETWEEN 2 AND 99 GROUP BY sector_id;
+
