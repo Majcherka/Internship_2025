0f6cd637cf227a1bb51f8710cedbbfa23976eb75
Majcherka <magdulka2003@gmail.com>
2025-05-11 21:52:57 +0200

    Add SQL code for Data Analysis and Visualisation task

---
+++ 2_2_Prediction_error_analysis.sql
@@ -0,0 +1,7 @@
+SELECT TIMESTAMPDIFF(SECOND, segment_start_time, segment_end_time) / 60 AS actual_delivery_time,
+       o.planned_delivery_duration / 60 AS planned_delivery_time,
+       ABS(o.planned_delivery_duration / 60 - TIMESTAMPDIFF(SECOND, segment_start_time, segment_end_time) / 60) AS prediction_error
+FROM route_segments rs
+JOIN orders o ON rs.order_id = o.order_id
+WHERE rs.segment_type = 'STOP';
+
